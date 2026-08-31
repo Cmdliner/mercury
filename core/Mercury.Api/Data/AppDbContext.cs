316346165d4c6fa1a6b5afa0e6c559561fecb937
Cmdliner <abiadeabdulazeez@gmail.com>
2026-08-31 12:44:55 +0100

    update(database): update seed data

--- core/Mercury.Api/Data/AppDbContext.cs
+++ core/Mercury.Api/Data/AppDbContext.cs
@@ -25,7 +25,7 @@ public class AppDbContext(DbContextOptions<AppDbContext> options): DbContext(opt
                 new Account { Id = Guid.Parse("22222222-2222-2222-2222-222222222222"), Code = "STORE-A-CASH-TILL", Name = "Store A – Cash Till", Type = AccountType.Asset },
                 new Account { Id = Guid.Parse("33333333-3333-3333-3333-333333333333"), Code = "PHARMACY-BANK", Name = "Pharmacy Bank Account", Type = AccountType.Asset },
                 new Account { Id = Guid.Parse("44444444-4444-4444-4444-444444444444"), Code = "SALES-REVENUE-A", Name = "Sales Revenue – Store A", Type = AccountType.Revenue },
-                new Account { Id = Guid.Parse("55555555-5555-5555-5555-555555555555"), Code = "REFUNDS-A", Name = "Refunds – Store A", Type = AccountType.Revenue }
+                new Account { Id = Guid.Parse("55555555-5555-5555-5555-555555555555"), Code = "REFUNDS-A", Name = "Refunds – Store A", Type = AccountType.Expense }
             );
             
         });
---
+++ core/Mercury.Api/Migrations/20260831113723_UpdateSeedAccounts.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mercury.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSeedAccounts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "type",
+                table: "accounts",
+                type: "character varying(16)",
+                maxLength: 16,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(32)",
+                oldMaxLength: 32);
+
+            migrationBuilder.UpdateData(
+                table: "accounts",
+                keyColumn: "id",
+                keyValue: new Guid("55555555-5555-5555-5555-555555555555"),
+                column: "type",
+                value: "Expense");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "type",
+                table: "accounts",
+                type: "character varying(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(16)",
+                oldMaxLength: 16);
+
+            migrationBuilder.UpdateData(
+                table: "accounts",
+                keyColumn: "id",
+                keyValue: new Guid("55555555-5555-5555-5555-555555555555"),
+                column: "type",
+                value: "Revenue");
+        }
+    }
+}
